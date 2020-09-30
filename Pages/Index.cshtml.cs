cf9810cbad30fdbcc5870332600c8d32f77d1a6e
ubarcturus <ubarcturus@gmail.com>
2020-09-30 16:43:19 +0200

    add search button to index site

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Wohnungsportal.Models;
 
 namespace Wohnungsportal.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
 
-        public void OnGet()
-        {
-        }
-    }
+		public List<Apartment> Apartments { get; set; }
+
+		public void OnGet()
+		{
+			Apartments = new List<Apartment>();
+		}
+	}
 }
