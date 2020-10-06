45534d23fdd41982bd9e62b7cbb3f7e70c02a7f2
Philipp <philipp@kauz.ch>
2020-10-06 16:48:45 +0200

    cleanup nested ifs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,36 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Wohnungsportal.Data;
 using Wohnungsportal.Models;
 
 namespace Wohnungsportal.Pages
 {
-	public class IndexModel : PageModel
-	{
-		private readonly ApplicationDbContext _context;
-		private readonly ILogger<IndexModel> _logger;
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<IndexModel> _logger;
 
-		public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
-		{
-			_logger = logger;
-			_context = context;
-		}
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-		public List<Apartment> Apartments { get; private set; }
+        public List<Apartment> Apartments { get; private set; }
 
-		public void OnGet()
-		{
-			try
-			{
-				Apartments = _context.Apartment.ToList();
-			}
-			catch (Exception exception)
-			{
-				_logger.LogWarning($"Och ne. {exception.Message}");
-			}
-		}
-	}
+        public async Task<PageResult> OnGetAsync()
+        {
+            try
+            {
+                Apartments = await _context.Apartment.ToListAsync();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogWarning($"Och ne. {exception.Message}");
+            }
+
+            return Page();
+        }
+    }
 }
