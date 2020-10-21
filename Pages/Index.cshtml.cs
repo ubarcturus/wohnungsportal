5da5fbb4ac4e91522ae75a6dfb423326cf60e1e9
Bill Staufer <bill.stauffer@kauz.ch>
2020-10-21 15:25:03 +0200

    Reservation Data format without time

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -39,6 +40,7 @@ namespace Wohnungsportal.Pages
 			                if (reservation.Start <= DateTime.Now.Date && reservation.End >= DateTime.Now.Date)
 			                {
 				                apartment.IsReserved = true;
+				                break;
 			                }
 		                }
 	                }
